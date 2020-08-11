c1a6fb6d805f333fcf562adc40ecb6cdeaf77854
tretherington <tretherington@users.noreply.github.com>
2020-08-11 14:18:46 +1200

    Revised grid_coordinates function

--- R/grid-coordinates.R
+++ R/grid-coordinates.R
@@ -1,25 +1,22 @@
-#-------------------------------------------------------------
 #' @title Grid Coordinates
-#' @description Create an \code{n}-dimensional grid of coordinates across space.
+#' 
+#' @description This function creates a \code{n}-dimensional grid of coordinates 
+#' systematically spaced throughout the specified spatial extent.
 #'
 #' @param mins Vector of length \code{n} listing the point space minimum for each
 #' dimension.
-#' @param maxs Vector of length \code{n} listing the pointspace maximum for each
+#' @param maxs Vector of length \code{n} listing the point space maximum for each
 #' dimension.
-#' @param nCoords Number of coordinates across the point space in all dimensions.
+#' @param spacing The spacings between points in all dimensions.
 #'
-#' @details This function creates a grid of coordinates systematically located
-#' throughout the specified point space to enable visualisation of alpha shape .
-#' The extent of the grid is given by the \code{mins} and \code{maxs}, and the number
-#' of coordinates for each dimension is given by \code{nCoords}.
-#'
-#' @return A matrix with \code{n} columns.
+#' @return A matrix with \code{n} columns - and potentially lots of rows!
 #'
 #' @examples
 #' # Point space grid coordinates usage
-#' xy = grid_coordinates(mins=c(15,0), maxs=c(35,200), nCoords=5)
+#' xy = grid_coordinates(mins=c(0,0), maxs=c(10,15), spacing=1)
+#' 
 #' @export
-grid_coordinates <- function(mins, maxs, nCoords) {
+grid_coordinates <- function(mins, maxs, spacing) {
   
   # Check input data
   if (length(mins) != length(maxs)) {
@@ -28,14 +25,14 @@ grid_coordinates <- function(mins, maxs, nCoords) {
   if (FALSE %in% (mins < maxs)) {
     stop("Maximums not greater than minimums in all dimensions")
   }
-  if (nCoords <= 1) {
-    stop("Sample size must be greater than one")
+  if (spacing <= 0) {
+    stop("Spacing must be greater than zero")
   }
   
   # Create list of coordinate locations for each dimension
   dimCoords = list()
   for (n in seq(1, length(mins))) {
-    dimCoords[[n]] = seq(mins[n], maxs[n], (maxs[n] - mins[n]) / (nCoords - 1))
+    dimCoords[[n]] <- seq(mins[n], maxs[n], spacing)
   }
   # Create all combinations of coordinates across all dimension
   return(expand.grid(dimCoords))
