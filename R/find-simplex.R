98021718530aa789faf6a8fbac9f1ac44d532607
LANDCARE\omondiagbep <omondiagbep@landcareresearch.co.nz>
2020-09-15 14:09:53 +1200

    optimising find-simplex function

--- R/find-simplex.R
+++ R/find-simplex.R
@@ -52,65 +52,56 @@ find_simplex <- function(simplices, test_points) {
 
   # Identify the simplices that test point belongs 
   # First check if the point lies in the convex hull
+  tic("dd")
   hull <- convex_hull(points = simplices$input_points)
   inHull <- in_convex_hull(hull, test_points)
   
   # Create an empty object to hold which simplex the test points are in
-  test_points_simplex <- NULL
+  test_points_simplex <- rep(NA, nrow(test_points)) 
   
-  # For each test point that is within the convex hull
-  for (p in c(1:nrow(test_points))) {
-    if (!is.na(inHull[p])) { # If it is not an NA
-      if (inHull[p] == TRUE) { # If it is in the convex hull
-        # Loop over the simplices
+
+  
+  #profvis({  #start profiling
+    
+  #tic("time") #track time
+  test_points_simplex <- rep(NA, nrow(test_points)) 
+  inHull_True=(which(inHull==TRUE)) #use which command to select the row which are true.
+  for(p in inHull_True) {
+        tp = rbind(c(test_points[p,]))
         for(s in c(1:nrow(simplices$simplices))) {
           
           # Get the coordinates of the points that make the simplex
           simplex_indices <- simplices$simplices[s, ]
           simplex_coordinates <- simplices$input_points[simplex_indices, ]
-  
+          
           # Get the barycentric coordinate of the test point for the simplex
-          tp = rbind(c(test_points[p,]))
-          test_barycentric <- barycentric_coordinate(simplex_coordinates, tp)
+     
+          test_barycentric <- barycentric_coordinate(simplex_coordinates, tp,1,ncol(tp))
           
           # The point is inside the triangle all of the barycentric coordinates 
           # are positive
           check_sign <- sign(test_barycentric[1, ])
-          if (length(which(check_sign == -1)) == 0) {
+          if(length(which(check_sign == -1)) == 0) {
             test_points_simplex[p] <- s
             break    # the test point is inside this simplex so stop searching
-          } else {
-            test_points_simplex[p] = NA # test point is not in a simplex
-          }
+          } 
         }
-      } else {
-        test_points_simplex[p] = NA # test point is not in a simplex
-      }
-    } else {
-      test_points_simplex[p] = NA # test point is not in a simplex
-    }
+ 
   }
+  # toc() #stop tracking time
+  #}) #end profiling
+
   return (test_points_simplex)
   #return(.Call("C_findSimplex", hull$convexhull, test_points, PACKAGE="compGeometeR"))
 }
 
 # Internal function used by \code{\link{find_simplex}} to calculate the 
 # barycentric coordinate of a point in relation to a simplex
-barycentric_coordinate <- function(X, P) {
-  M <- nrow(P)
-  N <- ncol(P)
-  if (ncol(X) != N) {
-    stop("Simplex X must have same number of columns as point matrix P")
-  }
-  if (nrow(X) != (N+1)) {
-    stop("Simplex X must have N columns and N+1 rows")
-  }
+barycentric_coordinate <- function(X, P,M,N) {
+
   X1 <- X[1:N,] - (matrix(1,N,1) %*% X[N+1,,drop=FALSE])
-  if (rcond(X1) < .Machine$double.eps) {
-    warning("Degenerate simplex")
-    return(NULL)
-  }
-  Beta <- (P - matrix(X[N+1,], M, N, byrow=TRUE)) %*% solve(X1)
+  Beta <- (P - matrix(X[N+1,], 1, N, byrow=TRUE)) %*% solve(X1)
   Beta <- cbind(Beta, 1 - apply(Beta, 1, sum))
+  
   return(Beta)
 }
